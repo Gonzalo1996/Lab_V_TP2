bef50360c9f186748b676652069278aad6c2c925
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-04-12 11:17:13 -0300

    creacion clase venta

---
+++ TP2_GRUPO_4/src/dominio/Venta.java
@@ -0,0 +1,80 @@
+package dominio;
+
+import java.util.Date;
+import java.util.List;
+import java.text.MessageFormat;
+
+public class Venta {
+
+	// Atributos
+	private int id;
+	private Date fecha;
+	private Cliente cliente; 
+	private double monto;
+	private List<Entrada> listaEntradas;
+
+	private static int contId = 0;
+	
+	
+	// Constructores
+	public Venta() {
+		this.id = ++contId;
+	}
+
+	public Venta(Date fecha, Cliente cliente, double monto, List<Entrada> listaEntradas) {
+		this.id = ++contId;
+		this.fecha = fecha;
+		this.cliente = cliente;
+		this.monto = monto;
+		this.listaEntradas = listaEntradas;
+	}
+
+	
+	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public double getMonto() {
+		return monto;
+	}
+
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+
+	public List<Entrada> getListaEntradas() {
+		return listaEntradas;
+	}
+
+	public void setListaEntradas(List<Entrada> listaEntradas) {
+		this.listaEntradas = listaEntradas;
+	}
+
+	
+	// toString()
+	@Override
+	public String toString() {
+		return MessageFormat.format("ID venta: {0}, Fecha: {1, date}, Cliente: ({2}), Monto: {3, number, currency}.", id, fecha, cliente, monto);
+	}
+}
--- TP2_GRUPO_4/src/resolucion/Ejercicio1.java
+++ TP2_GRUPO_4/src/resolucion/Ejercicio1.java
@@ -1,10 +1,19 @@
 package resolucion;
 
+import java.util.Date;
+
+import dominio.Cliente;
+import dominio.Venta;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cliente cliente = new Cliente();
+		Date fecha = new Date();
+		
+		Venta vta = new Venta(fecha, cliente, 399.99, null);
+		
+		System.out.println(vta.toString());
 	}
 
 }
